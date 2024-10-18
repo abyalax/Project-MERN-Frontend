7f88162273e88913bdb186f49d59bd39a2bfbd4a
abyalax <abyalaxx@gmail.com>
2024-10-18 08:41:49 +0700

    Logging all env injected at production

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -2,4 +2,5 @@ const originDevelopment = import.meta.env.VITE_ORIGIN_DEVELOPMENT
 const originProduction = import.meta.env.VITE_ORIGIN_PRODUCTION
 
 export const origin = originProduction ?? originDevelopment
+console.log("Available ENV: ",import.meta.env);
 console.log(origin, "From Utils Service");
