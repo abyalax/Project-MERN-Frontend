4eb9af538c1f00c5de98396b4c38539312dbbe16
abyalax <abyalaxx@gmail.com>
2024-11-05 06:55:56 +0700

    Forgot to change the config for production

--- src/utils/constant.ts
+++ src/utils/constant.ts
@@ -1,8 +1,7 @@
-const originDevelopment = import.meta.env.VITE_ORIGIN_DEVELOPMENT
-// const originProduction = import.meta.env.VITE_ORIGIN_PRODUCTION
+// const originDevelopment = import.meta.env.VITE_ORIGIN_DEVELOPMENT
+const originProduction = import.meta.env.VITE_ORIGIN_PRODUCTION
 
-export const origin = originDevelopment
+export const origin = originProduction
 
 export const token = localStorage.getItem("token")
-console.log("Token: ", token);
 
